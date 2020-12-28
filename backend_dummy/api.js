9ddd43a0b52d9d32bbfb016770098987226fb68a
thanaphon0737 <35342731+thanaphon0737@users.noreply.github.com>
2020-12-28 21:49:58 +0700

    files that can cause bug was deleted.

--- backend_dummy/api.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use(require("./api_auth"))
-router.use(require("./api_product"))
-
-module.exports = router;
--- backend_dummy/api_auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/login", (req, res)=>{
-  res.json({result: "Login"})
-})
-module.exports = router;
